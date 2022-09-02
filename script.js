5671d66cd5b67af902a1fbadafc4d366a41ae8b6
Bruno Laguna <bruno_cs1.8@hotmail.com>
2022-09-02 16:00:35 -0300

    8° commit - branch F1
    - Added some functions for the responsiveness of some elements and added the slider buttons to move to the next image and back.

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
 let count = 1
 document.getElementById("radio1").checked = true
-    
+
+let manual_btn = document.querySelector('.manual-btn')
+console.log(manual_btn)
+
+//function label_click(){
+//    count = 0
+//    setInterval( function(){
+//    nextImage()
+//}, 1000)
+//}
+
 setInterval( function(){
     nextImage()
-}, 10000)
+}, 1000)
 
+button_lt = document.getElementById('button_lt')
+button_gt = document.getElementById('button_gt')
 function nextImage(){
     if(count<4) {
         count++
         document.getElementById("radio"+count).checked = true
+        if(count === 4){
+            button_lt.style.color = '#415691'
+            button_gt.style.color = '#415691' 
+        } //else if(count === 3){
+       //    button_lt.style.color = '#AA2323'
+       //    button_gt.style.color = '#AA2323'
+       //} else if(count === 2){
+       //    button_lt.style.color = '#AA2323'
+       //    button_gt.style.color = '#AA2323'
+       //} else if(count === 1){
+       //    button_lt.style.color = '#AA2323'
+       //    button_gt.style.color = '#AA2323'
+       //} 
+    } else {
+        return
     } 
 }
 
+var nautic_line = document.getElementById("radio4").checked
+button = document.querySelector(".button")
+console.log(nautic_line)
+console.log(button)
+if(nautic_line === true){
+    button.style.color = 'blue'
+    console.log(nautic_line)
+}
